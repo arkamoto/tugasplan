6ef041374d8682899b986b8793fdf2c495846524
Arief Kamaluddin <ariefkamaluddin.ak96@gmail.com>
2026-01-22 13:35:22 +0700

    fix: add fallback for API_ROOT in getImageUrl

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,13 +1,16 @@
+const FALLBACK_API_URL = "https://be-sporton.agunacourse.com/api";
+const FALLBACK_API_ROOT = "https://be-sporton.agunacourse.com";
+
 export async function fetchAPI<T>(
   endpoint: string,
   options?: RequestInit,
 ): Promise<T> {
-  const fallbackApiUrl = `https://be-sporton.agunacourse.com/api`;
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || fallbackApiUrl;
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || FALLBACK_API_URL;
   const res = await fetch(`${apiUrl}${endpoint}`, {
     ...options,
     cache: options?.cache || "no-store",
   });
+
   if (!res.ok) {
     let errorMessage = `Failed to fetch data from ${endpoint}`;
     try {
@@ -25,5 +28,7 @@ export async function fetchAPI<T>(
 
 export function getImageUrl(path: string) {
   if (path.startsWith("http")) return path;
-  return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`;
+
+  const apiRoot = process.env.NEXT_PUBLIC_API_ROOT || FALLBACK_API_ROOT;
+  return `${apiRoot}/${path}`;
 }
